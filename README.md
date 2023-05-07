a80dad00fb2e0eb622e21efc9e101f1f99a0439d
Yancharuk Alexander <alex@itvault.info>
2023-05-07 14:25:22 +0300

    feat: Implement zapper with base functionality

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# zapper
+# Uber Zap logger helpers
+[![GitHub release][Release img]][Release src] [![Conventional Commits][Conventional commits badge]][Conventional commits src]
+
+## Conf
+Features:
+- easiest Zap Logger configuration using yaml-config.
+- configuration TCP and UDP Zap Logger sinks out of box.
+- minimal code for full-featured Zap Logger initialization. 
+
+### Getting started
+
+```go
+package main
+
+import (
+	"log"
+	
+	"github.com/nafigator/zapper"
+	"go.uber.org/zap"
+)
+
+func main() {
+	var zl *zap.SugaredLogger
+	var err error
+
+	if zl, err = zapper.New(nil, nil); err != nil {
+		log.Fatal("Zap logger failure: ", err)
+	}
+	
+	zl.Info("Zap logger: OK")
+}
+```
+
+### Default configuration:
+```yaml
+level: info
+encoding: console
+outputPaths:
+  - stdout
+errorOutputPaths:
+  - stderr
+encoderConfig:
+  messageKey: message
+  levelKey:   level
+  timeKey:    time
+  callerKey:  line
+  levelEncoder: capitalColor
+  timeEncoder:
+    layout: 2006-01-02 15:04:05.000
+  durationEncoder: string
+  callerEncoder: default
+```
+
+By default, package search yaml-config in path `/etc/zap/config.yaml`.
+Optionally there is ability to parametrize custom path to config using path-param in `zapper.New(nil, "/custom/path")`.
+
+If zapper initialized with fallback logger, failures will be logged and Zap Logger will be initialized with default
+configuration.
+
+
+
+[Release img]: https://img.shields.io/badge/release-0.1.0-red.svg
+[Release src]: https://github.com/nafigator/zap
+[Conventional commits src]: https://conventionalcommits.org
+[Conventional commits badge]: https://img.shields.io/badge/Conventional%20Commits-1.0.0-yellow.svg
---
+++ zapper.go
@@ -0,0 +1,111 @@
+package zapper
+
+import (
+	"io"
+	"os"
+
+	_ "github.com/kontera-technologies/zap-net-sink"
+	"go.uber.org/zap"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	openConfigWarning = "Zap config open failure: %s. Fallback to default"
+	readConfigWarning = "Zap config read failure: %s. Fallback to default"
+	defaultConfPath   = "/etc/zap/config.yaml"
+	defaultConf       = `
+level: info
+encoding: console
+outputPaths:
+  - stdout
+errorOutputPaths:
+  - stderr
+encoderConfig:
+  messageKey: message
+  levelKey:   level
+  timeKey:    time
+  callerKey:  line
+  levelEncoder: capitalColor
+  timeEncoder:
+    layout: 2006-01-02 15:04:05.000
+  durationEncoder: string
+  callerEncoder: default
+`
+)
+
+type fallbackLogger interface {
+	Printf(string, ...any)
+}
+
+// New creates logger instance.
+func New(fl fallbackLogger, path *string) (*zap.SugaredLogger, error) {
+	var cfg zap.Config
+	var logger *zap.Logger
+	var err error
+
+	if err = yaml.Unmarshal(getYaml(fl, path), &cfg); err != nil {
+		return nil, err
+	}
+
+	if logger, err = cfg.Build(); err != nil {
+		return nil, err
+	}
+
+	return logger.Sugar(), nil
+}
+
+// Must function creates logger instance without error returning.
+func Must(fl fallbackLogger, path *string) *zap.SugaredLogger {
+	var cfg zap.Config
+	var logger *zap.Logger
+	var err error
+
+	if err = yaml.Unmarshal(getYaml(fl, path), &cfg); err != nil {
+		if fl != nil {
+			fl.Printf("Zapper unmarshal failure: %s", err)
+		}
+
+		l, _ := zap.NewProductionConfig().Build()
+
+		return l.Sugar()
+	}
+
+	if logger, err = cfg.Build(); err != nil {
+		l, _ := zap.NewProductionConfig().Build()
+
+		return l.Sugar()
+	}
+
+	return logger.Sugar()
+}
+
+func getYaml(fl fallbackLogger, path *string) []byte {
+	var bytes []byte
+	var err error
+	var yamlFile *os.File
+	var p string
+
+	if path != nil {
+		p = *path
+	} else {
+		p = defaultConfPath
+	}
+
+	if yamlFile, err = os.Open(p); err != nil {
+		if fl != nil {
+			fl.Printf(openConfigWarning, err)
+		}
+
+		return []byte(defaultConf)
+	}
+
+	if bytes, err = io.ReadAll(yamlFile); err != nil {
+		if fl != nil {
+			fl.Printf(readConfigWarning, err)
+		}
+
+		return []byte(defaultConf)
+	}
+
+	return bytes
+}
